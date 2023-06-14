65deccbcbdb5377a682d90d022983379c5ec19dd
marcellfischbach <marcellfischbach@users.noreply.github.com>
2023-06-14 13:53:34 +0200

    Integration of java is working... now every class needs to be implemented.

---
+++ Java/CrimsonEdge/src/main/java/org/crimsonedge/core/BaseObject.java
@@ -0,0 +1,14 @@
+package org.crimsonedge.core.entity;
+
+public class BaseObject {
+
+    protected long refID;
+
+    public long getRefID() {
+        return refID;
+    }
+
+    public void setRefID(long refID) {
+        this.refID = refID;
+    }
+}
